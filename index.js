46037201f25907c944d12c6300e2e2fe81ecb96f
Brandon <brandon.uan@gmail.com>
2022-11-22 21:43:47 -0500

    corsOptions added

--- index.js
+++ index.js
@@ -8,13 +8,23 @@ import userRoutes from "./src/routes/userRoutes.js";
 
 import { PORT } from "./src/config.js";
 const app = express();
+const whiteList = ["http://localhost:3000"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("No habilitado por CORS"));
+    }
+  },
+};
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/auth", authRoutes);
-app.use("/api", wordRoutes);
-app.use("/api", userRoutes);
+app.use("/auth", cors(corsOptions), authRoutes);
+app.use("/api", cors(corsOptions), wordRoutes);
+app.use("/api", cors(corsOptions), userRoutes);
 
 app.listen(PORT);
 console.log(`Server listen on port ${PORT}`);
